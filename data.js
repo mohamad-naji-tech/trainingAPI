f6c2c197c550e5b8ef664c57568f474b25eb2645
mohamad naji <you@example.com>
2024-08-03 08:56:28 +0300

    we again catch error

--- data.js
+++ data.js
@@ -1,11 +1,5 @@
-//const express = require('express')
-//const app = express()
 
-//app.get('/', function (req, res) {
- // res.send('Hello World')
-//})
 
-//app.listen(3000)
 const express = require("express");
 const cors=require ('cors')
 const app = express();
@@ -16,11 +10,10 @@ let data=xlsx.utils.sheet_to_json(ws);
 app.use(cors());
 const PORT = process.env.PORT || 3030;
 
-//app.get('/', function (req, res) {
-  //res.send('Hello World')})
-  app.get('/students', function (req, res) {
+
+  app.get('/teams', function (req, res) {
     res.send(data)});
-// your code
+
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
